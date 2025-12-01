a5e06b1dcb9be7b75c6f7aef7387472fb034818d
Anil Sebastian <seanil@microsoft.com>
2025-11-30 20:00:21 -0800

    Remove use client directive from privacy and terms pages - should be server components

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -2,10 +2,6 @@
 
 import Link from 'next/link';
 
-"use client";
-
-import Link from 'next/link';
-
 export default function PrivacyPage() {
   return (
     <main className="max-w-3xl mx-auto px-4 py-16">
